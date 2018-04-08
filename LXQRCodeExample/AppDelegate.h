fc6ff8fd81d1c6e45e5db7d73498b74ba9df8075
雪晟 <missli_211@163.com>
2018-04-08 14:30:21 +0800

    Initial Commit

---
+++ LXQRCodeExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXQRCodeExample
+//
+//  Created by 漫漫 on 2018/4/8.
+//  Copyright © 2018年 漫漫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
